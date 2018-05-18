6b8891c73fd7e30915fbc9d262fb01c1f56a029a
Amir Taheri <amir.houshang.taheri@gmail.com>
2018-05-19 00:46:24 +0200

    Some changes

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -1,7 +1,18 @@
-async function main() {
+/*async function main() {
   const response = await fetch('api/entries');
   const { data } = await response.json();
   console.log(data);
 }
 
-//main();
+//main();*/
+
+async function main() {
+  fetch('api/entries').
+  then((response) => response.json()).
+  then(function(data){
+    debugger;
+    console.log(data[1].toString());
+    });
+}
+
+main();
